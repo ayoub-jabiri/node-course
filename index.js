7e9a5fe2909bc782e22a6826b3b3291071b4a054
ayoub-jabiri <ayoubjabiri.business@gmail.com>
2026-03-25 12:48:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("node:fs");
 
 const content = fs.readFileSync("./txt/input.txt");
 
+console.log("Start...")
+
 console.log("File content: ", content.toString());
 
 console.log("Done!")
