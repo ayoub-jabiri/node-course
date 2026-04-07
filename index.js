f2f0c3bd0c0344cdabb9a480bfb5c7ea0d943000
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-04-07 11:29:55 +0100

    Building the HTML Templates

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+const http = require("node:http");
 const fs = require("node:fs");
 
-const content = fs.readFileSync("./txt/input.txt");
+const objData = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 
-console.log("Start");
+const server = http.createServer((req, res) => {
+    const path = req.url;
 
-console.log(content.toString());
+    if (path == "/" || path == "/overview") {
+        res.end("This is the overview");
+    } else if (path == "/product") {
+        res.end("This is the product");
+    } else if (path == "/api") {
+        res.writeHead(200, {
+            "content-type": "application/json",
+        });
+
+        res.end(objData);
+    } else {
+        res.writeHead(404, {
+            "content-type": "text/html",
+        });
+        res.end("<h1>Page not found!</h1>");
+    }
+});
+
+server.listen(3000, "127.0.0.1", () => {
+    console.log("The server is listening on port 3000");
+});
